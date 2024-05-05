0a2b4d24f169bcd6da1bcd189dc4e6f1cba86107
Unruh-Spirale <protjan@wp.pl>
2024-05-05 11:17:23 +0200

    add form/textarea on ViewNote view

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,9 +1,25 @@
 <template>
     <div class="notes">
+
+        <div class="card has-background-success-dark p-4 mb-5">
+            <div class="field">
+                <div class="control">
+                    <textarea class="textarea" placeholder="Add a new note" />
+                </div>
+            </div>
+            <div class="field is-grouped is-grouped-right">
+                <div class="control">
+                    <button class="button is-link has-background-success">Add New Note</button>
+                </div>
+            </div>
+        </div>
+
         <div v-for="i in 3" class="card mb-4">
             <div class="card-content">
                 <div class="content">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum ullam in labore obcaecati quis vitae, ea qui quos illo dicta nisi deleniti! Nisi, aliquam necessitatibus suscipit odio excepturi ex recusandae.
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum ullam in labore obcaecati quis
+                    vitae, ea qui quos illo dicta nisi deleniti! Nisi, aliquam necessitatibus suscipit odio excepturi ex
+                    recusandae.
                 </div>
             </div>
             <footer class="card-footer">
