5e44fc0d926aa9c58e2764d404f5ceb87e8f31a2
Unruh-Spirale <protjan@wp.pl>
2024-05-05 10:52:51 +0200

    add notes page design

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,5 +1,15 @@
 <template>
     <div class="notes">
-        <h1>Notes</h1>
+        <div v-for="i in 3" class="card mb-4">
+            <div class="card-content">
+                <div class="content">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum ullam in labore obcaecati quis vitae, ea qui quos illo dicta nisi deleniti! Nisi, aliquam necessitatibus suscipit odio excepturi ex recusandae.
+                </div>
+            </div>
+            <footer class="card-footer">
+                <a href="#" class="card-footer-item">Edit</a>
+                <a href="#" class="card-footer-item">Delete</a>
+            </footer>
+        </div>
     </div>
 </template>
