bbe2cd1ba5683f58a56fc8cd2c56f33f96342628
Unruh-Spirale <protjan@wp.pl>
2024-05-05 10:03:48 +0200

    Install Bulma

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
-  <RouterLink to="/">Notes</RouterLink> 
-  <RouterLink to="/stats">Stats</RouterLink>
+  <RouterLink to="/" class="button">Notes</RouterLink> 
+  <RouterLink to="/stats" class = "button">Stats</RouterLink>
 
   <RouterView />
 </template>
+
+<style>
+@import 'node_modules\bulma\css\bulma.min.css';
+</style>
 
 
