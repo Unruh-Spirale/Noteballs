6b6caeb7c98d8535f1cbb2a559a620353bc239b1
Unruh-Spirale <protjan@wp.pl>
2024-05-05 08:53:36 +0200

    Router - Tidying Up

--- src/main.js
+++ src/main.js
@@ -1,27 +1,8 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
-import ViewNotes from '@/views/ViewNotes.vue'
-import ViewStats from '@/views/ViewStats.vue'
-
-const router = createRouter({
-    history: createWebHashHistory(),
-    routes: [
-        {
-            path: '/',
-            name: 'notes',
-            component: ViewNotes
-        },
-        {
-            path: '/stats',
-            name: 'stats',
-            component: ViewStats
-        },
-
-    ]
-})
+import router from '@/router/index.js'
 
 createApp(App)
 .use(router)
