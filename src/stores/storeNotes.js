d8d58dceed183b6136bff0f2fc939a46e4a43186
Unruh-Spirale <protjan@wp.pl>
2024-05-05 13:28:48 +0200

    instal and setup Pinia, create store with notes

---
+++ src/stores/storeNotes.js
@@ -0,0 +1,19 @@
+import { defineStore } from 'pinia'
+
+export const useStoreNotes = defineStore('storeNotes', {
+    state: () => {
+        return {
+            notes: [
+                {
+                    id: 'id1',
+                    content: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum ullam in labore obcaecati quis vitae, ea qui quos illo dicta nisi deleniti! Nisi, aliquam necessitatibus suscipit odio excepturi ex recusandae.'
+                },
+                {
+                    id: 'id2',
+                    content: 'This is a shorter note! Woo!'
+                }
+            ]
+        }
+    },
+
+})
